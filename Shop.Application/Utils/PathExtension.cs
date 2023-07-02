0974059b0f6a2b59942048360e6f4287471ed15a
hadisoufi00 <hadisoufi00@gmail.com>
2023-07-02 18:34:02 +0330

    Add Utils To Application Layer And Add PathExtension And TextFixer To Utils

---
+++ Shop.Application/Utils/PathExtension.cs
@@ -0,0 +1,29 @@
+﻿namespace Shop.Application.Utils
+{
+    public class PathExtension
+    {
+        #region domain address
+
+        public static string DomainAddress = "http://localhost:5196";
+
+        #endregion
+
+        #region default images
+
+        public static string DefaultAvatar = "/Template/Images/Defaults/";
+
+        #endregion
+
+        #region user
+
+        public static string UserAvatarOrigin = "/Template/Images/User/Origin/";
+        public static string UserAvatarOriginServer =
+            Path.Combine(Directory.GetCurrentDirectory(), "wwwroot/Template/Images/User/Origin/");
+
+        public static string UserAvatarThumb = "/Template/Images/User/Thumb/";
+        public static string UserAvatarThumbServer =
+            Path.Combine(Directory.GetCurrentDirectory(), "wwwroot/Template/Images/User/Thumb/");
+
+        #endregion
+    }
+}
