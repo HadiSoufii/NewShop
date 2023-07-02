3d25599855ce57269f86c4c7701e18faf0653397
hadisoufi00 <hadisoufi00@gmail.com>
2023-07-02 18:22:56 +0330

    Add GenericRepository

---
+++ Shop.Domain/Interfaces/IGenericRepository.cs
@@ -0,0 +1,15 @@
+﻿using Shop.Domain.Models.Common;
+
+namespace Shop.Domain.Interfaces
+{
+    public interface IGenericRepository<T> where T : BaseEntity
+    {
+        Task<IEnumerable<T>> GetEntityAllAsync();
+        Task<T?> GetEntityByIdAsync(int id);
+        Task<bool> IsEixstEntityByIdAsync(int id);
+        Task<int> AddAsync(T entity);
+        Task UpdateAsync(T entity);
+        Task DeleteAsync(int id);
+        Task DeleteAsync(T entity);
+    }
+}
