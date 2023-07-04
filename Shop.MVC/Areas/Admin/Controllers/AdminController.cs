02fff3040a3d96de7c481f6bdab23d50529ae5b3
mohammad amin rahmati <mohammadaminrahmati@outlook.com>
2023-07-04 16:35:42 +0330

    complete admin

---
+++ Shop.MVC/Areas/Admin/Controllers/AdminController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Shop.MVC.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class AdminController : Controller
+    {
+
+        [HttpGet("admin")]
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
